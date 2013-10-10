58ca5a0f250dd46bb6ae1b443bf3e65a76e19cca
Andreas Bruun Okholm <aokholm@gmail.com>
2013-10-10 07:28:56 +0200

    added htttpresponseredirect import to urls.py

--- mpcd_web/urls.py
+++ mpcd_web/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from mpcd_web.admin import admin_site
+from django.http import HttpResponseRedirect
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
