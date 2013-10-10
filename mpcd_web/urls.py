88c0a88a338f7eb61f3d67a1887244ffca347cb6
Andreas Bruun Okholm <aokholm@gmail.com>
2013-10-10 07:27:54 +0200

    Added url redirect on frontpage

--- mpcd_web/urls.py
+++ mpcd_web/urls.py
@@ -16,4 +16,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'myadmin/', include(admin_site.urls)),
+    url(r'^$', lambda r : HttpResponseRedirect('myadmin/')),
 )
