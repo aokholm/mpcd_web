8ca67825fea94719dbb9a00aa308caee52a7fbaf
Andreas Bruun Okholm <aokholm@gmail.com>
2013-10-10 08:22:54 +0200

    Owerrite debug tool bar show_toolbar_callback function

--- mpcd_web/settings.py
+++ mpcd_web/settings.py
@@ -169,6 +169,12 @@ LOGGING = {
 
 INTERNAL_IPS = ('127.0.0.1','130.226.161.122','130.225.93.176')
 
+def custom_show_toolbar(request):
+    return True  # Always show toolbar, for example purposes only.
+
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS' : False,
+    'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
 }
+
+
