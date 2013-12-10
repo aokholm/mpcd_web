0e4358ad324397f3f9396224e1887db40c8b1444
Andreas Bruun Okholm <aokholm@gmail.com>
2013-12-10 21:08:51 +0100

    Changed ITGrade to PCFunctions

--- mesdata/PCfunctions.py
+++ mesdata/PCfunctions.py
@@ -3,16 +3,16 @@ import numpy as np
 
 """Conversion funciton between ITgrade and Symtolerance - Input Should be in millimeters"""
 	
-def dimItg2symSymtol(Dim, ITG):
+def dimItg2Symtol(Dim, ITG):
 
 	# Calculates the symmetric Symtolerances from IT-grade
 	if Dim  <= 500:
-		Symtol = (10**(0.2*(ITG-1)))*(0.45*(Dim**(1./3.))+(0.001*Dim))/1000/2
+		symtol = (10**(0.2*(ITG-1)))*(0.45*(Dim**(1./3.))+(0.001*Dim))/1000/2
 	else:
-		Symtol = 10**(0.2*(ITG-1))*(0.004*Dim+2.1)/1000/2
-	return Symtol
+		symtol = 10**(0.2*(ITG-1))*(0.004*Dim+2.1)/1000/2
+	return symtol
 
-def dimSymtol2itg(Dim, Symtol):
+def dimSymtol2Itg(Dim, Symtol):
 
 	# IT-grade from symmetric Symtolerance
 
@@ -26,15 +26,15 @@ def dimSymtol2itg(Dim, Symtol):
 def stdMeanshiftCpk2Symtol(std,meanshift, cpk):
 
 	# Calculates the symmetric Symtolerance from cpk
-	d = meanshift + 3 * std * cpk
-	return d
+	symtol = abs(meanshift) + 3 * std * cpk
+	return symtol
 
-def dimStdMeanshiftCpk2itg(dim,std,meanshift,cpk):
+def dimStdMeanshiftCpk2Itg(dim,std,meanshift,cpk):
 
 	# Calculates a IT grade from meanshift, stadard deviation and cpk-value
 
-	Symtol = stdMeanshiftCpk2Symtol(std,meanshift,cpk)
+	symtol = stdMeanshiftCpk2Symtol(std,meanshift,cpk)
 
-	itg = dimSymtol2itg(dim,Symtol)
+	itg = dimSymtol2Itg(dim,symtol)
 
 	return itg
